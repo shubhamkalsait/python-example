4138a2e492560e037e2f3718f195d554780ddb17
skalsait <skalsait@dyl.com>
2024-03-21 12:50:34 +0530

    --- Auto Git Commit ---

--- stop_instance.py
+++ stop_instance.py
@@ -6,3 +6,12 @@ client = boto3.client('ec2')
 #     InstanceIds=['i-03f3abf9ce89c4cd6', 'i-03f3abf9ce89c4cd6' ]
 # )
 
+response = client.describe_instances(
+    Filters=[
+        {
+            'Name': 'string',
+            'Values': [
+                'string',
+            ]
+        },
+    ],
