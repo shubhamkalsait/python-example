c93527f2ca6cb663a6d9e506901e5f4ed913edc4
skalsait <skalsait@dyl.com>
2024-03-21 12:42:32 +0530

    --- Auto Git Commit ---

--- stop_instance.py
+++ stop_instance.py
@@ -2,7 +2,7 @@ import boto3
 
 client = boto3.client('ec2')
 
-response = client.stop_instances(
-    InstanceIds=['i-03f3abf9ce89c4cd6', 'i-03f3abf9ce89c4cd6' ]
-)
+# response = client.stop_instances(
+#     InstanceIds=['i-03f3abf9ce89c4cd6', 'i-03f3abf9ce89c4cd6' ]
+# )
 
